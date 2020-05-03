f5ee2cb013ebc1af13ac3ea2d1e15d8ef3da0f2f
Richie Carey <richiecarey@gmail.com>
2020-05-03 12:24:18 -0400

    Updates.

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: "./src/javascript/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
